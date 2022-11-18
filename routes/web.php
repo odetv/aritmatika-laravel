ed6b63af6be15a0b42a3c972c9adf93349672de3
odetv <abdiutama02@gmail.com>
2022-11-18 18:43:10 +0800

    Update file

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use App\Http\Controllers\SecantController;
 use App\Http\Controllers\KalkulatorController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 // Route standar menampilkan menu awal
 Route::get('/', function () {
     return view('home');
